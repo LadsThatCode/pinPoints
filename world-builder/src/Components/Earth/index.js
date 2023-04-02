fe42aab3704f040807e71d5f46e50c766acc93a8
Andrew Vreeland <andrewvreeland@gmail.com>
2023-04-01 23:50:48 -0400

    lat and lon bug fixed, last feature is the popups on hover or click for the points on the map, then maybe someanimations for the points.

--- world-builder/src/Components/Earth/index.js
+++ world-builder/src/Components/Earth/index.js
@@ -1,3 +1,4 @@
+
 import React, { useRef } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
@@ -9,34 +10,74 @@ import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import { TextureLoader } from "three";
 
+//! image loading 
 export function Earth(props) {
   const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
     TextureLoader,
     [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
-    );
-    
-    let lat = 90.0000
-    let lng = 45.0000
-  let radius = 1
+  );
+
+
   const earthRef = useRef();
   const cloudsRef = useRef();
-  let phi = (90 - lat) * (Math.PI / 180);
-  let theta = (lng + 180) * (Math.PI / 180);
-  let x = -((radius) * Math.sin(phi) * Math.cos(theta));
-    let z = ((radius) * Math.sin(phi) * Math.sin(theta));
-    let y = ((radius) * Math.cos(phi));
-  // let x = Math.cos(lng) * Math.sin(lat)
-  // let y = Math.sin(lng) * Math.sin(lat)
-  // let z = Math.cos(lat)
+  
+  
+  //! LAT AND LON INFO Two of the coordinates work out of the xyz and one doesnt, 
+  //TODO: TRY these coordinates to show proof of pevious comment 
+  //TODO: North Pole: 90.0000° N, 135.0000° W
+  //TODO: South Pole: 90.0000° S, 45.0000° E
+  //TODO: outside of the south west of africa: lat: 0°, lon: 0°
+  //! lat and lon anywhere near europe asia and africa work but the Americas/Hawai and south pole does not work.
 
+
+  const locations = [
+    { name: "tampa_Fl", latitude: 27.9506, longitude: -82.4572 },
+    { name: "california_Us", latitude: 36.7783, longitude: -119.4179 },
+    { name: "yuma_Az", latitude: 32.6927, longitude: -114.6277 },
+    { name: "seattle_Wa", latitude: 47.6062, longitude: -122.3321 },
+    { name: "denver_Co", latitude: 39.7392, longitude: -104.9903 },
+    { name: "newYorkCity_Ny", latitude: 40.7128, longitude: -74.0060 },
+    // Add more locations here
+  ];
+
+  // let lat = 27.9529744
+  // let lon = -82.4508736
+  // let radius = 1
+
+
+  function getCoordinates(latitude, longitude, radius) {
+    const phi = (90 - latitude) * Math.PI / 180;
+    const theta = (longitude + 180) * Math.PI / 180;
+    const x = -(radius * Math.sin(phi) * Math.cos(theta));
+    const y = radius * Math.cos(phi);
+    const z = radius * Math.sin(phi) * Math.sin(theta);
+    return [x, y, z];
+  }
+
+
+
+  // let newLocationData = locations.map(location => {
+
+  //   let phi = (90 - location.latitude) * (Math.PI / 180);
+  //   let theta = (location.longitude + 180) * (Math.PI / 180);
+  //   let x = -((radius) * Math.sin(phi) * Math.cos(theta));
+  //   let z = ((radius) * Math.sin(phi) * Math.sin(theta));
+  //   let y = ((radius) * Math.cos(phi));
+  //   console.log({ x, y, z })
+  //   return { x, y, z }
+  // })
+  // console.log(newLocationData)
+
+
+  //! ROTATION FOR EARTH AND CLOUDS
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
 
-    earthRef.current.rotation.y = elapsedTime / 7.5;
-    cloudsRef.current.rotation.y = elapsedTime / 6.5;
+    earthRef.current.rotation.y = elapsedTime / 30.5;
+    cloudsRef.current.rotation.y = elapsedTime / 23.5;
   });
 
-
+  //! Rotational AMBIET LIGHTING
   return (
     <>
       <ambientLight intensity={.5} />
@@ -49,7 +90,11 @@ export function Earth(props) {
         saturation={0}
         fade={true}
       />
+
+      [//! cloud mesh ]
+
       <mesh ref={cloudsRef} position={[0, 0, 0]}>
+
         <sphereGeometry args={[1.005, 1150, 1150]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -60,6 +105,7 @@ export function Earth(props) {
         />
       </mesh>
 
+      [//! Sphere mesh]
 
       <mesh ref={earthRef} position={[0, 0, 0]}>
         <sphereGeometry args={[1, 1150, 1150]} />
@@ -78,15 +124,30 @@ export function Earth(props) {
           panSpeed={0.5}
           rotateSpeed={0.4}
         />
-        <mesh position={[x, y, z]}>
-          <sphereBufferGeometry args={[0.009, 30, 30]} />
-          <meshBasicMaterial
-            attach="material"
-            color='red'
-            opacity={0.5}
-            transparent
-          />
-        </mesh>
+        [//! Pin Point Mesh]
+
+
+        {locations.map(location => (
+          <mesh key={location.name} position={getCoordinates(location.latitude, location.longitude, 1)}>
+            <sphereBufferGeometry args={[0.009, 30, 30]} />
+            <meshBasicMaterial
+              attach="material"
+              color="red"
+              opacity={0.5}
+              transparent
+            />
+          </mesh>
+        ))}
+
+        {/* <mesh>
+            <sphereBufferGeometry args={[0.009, 30, 30]} />
+            <meshBasicMaterial
+              attach="material"
+              color='red'
+              opacity={0.5}
+              transparent
+            />
+          </mesh> */}
 
       </mesh>
     </>
