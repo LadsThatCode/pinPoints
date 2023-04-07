8ee13a9661536ba147cf0759ac0db9c9943d046e
Logan Reese <lreese2112@gmail.com>
2023-04-07 05:33:41 +0000

    landing page login

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -14,19 +14,19 @@ export const Button = ({
 }) => {
   const checkButtonStyle = Styles.includes(buttonStyle) ? buttonStyle : Styles[0];
   const checkButtonSize = Sizes.includes(buttonSize) ? buttonSize : Sizes[0]
-
+  
   return (
     <>
 
-      <Link to='/search' className='btn--medium:hover'>
+      <Link to='/search' className='btn--medium:hover' buttonStyle='btn--medium:hover'>
         <button
           className={`btn ${checkButtonStyle} ${checkButtonSize}`}
           onClick={onClick}
           type={type}
         >
-          Pin Now!
+        I DONT WANT TO LOGIN I WANT TO PIN
         </button>
       </Link>
     </>
   );
-};
+}
